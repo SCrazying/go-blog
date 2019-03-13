8301ae93329d8a2e6115211198ea98e77f20141a
SCrazying <locojyw@outlook.com>
2019-03-13 20:58:12 +0800

    marge

--- config/Config.go
+++ config/Config.go
@@ -9,49 +9,42 @@ import (
 )
 
 type DbConfig struct {
-	Type string				//数据库连接类型
-	Host string				//数据库连接地址
-	Port string				//数据库连接端口
+	Type string //数据库连接类型
+	Host string //数据库连接地址
+	Port int    //数据库连接端口
 }
 
-type  HttpServerConfig struct {
+type HttpServerConfig struct {
 	Host string
-	Port string
+	Port int
 }
 
 type Config struct {
 	DbConfig
 	HttpServerConfig
 }
-var cfg Config
 
-func init(){
+func (c *Config) GetHttpAddr() string {
+	return fmt.Sprintf("%s:%d", c.HttpServerConfig.Host, c.HttpServerConfig.Port)
+}
 
-	file ,err := os.Open("./config.yaml")
-	if err!=nil{
+var Cfg = &Config{}
+
+func init() {
+
+	file, err := os.Open("./config.yaml")
+	if err != nil {
 		log.Fatalln(err)
-	} else{
-		yaml_data,err := ioutil.ReadAll(file)
-		if err!= nil{
+	} else {
+		yaml_data, err := ioutil.ReadAll(file)
+		if err != nil {
+			fmt.Println("无法读取配置文件 , ", err)
 			log.Fatalln(err)
 		}
-		err = yaml.Unmarshal(yaml_data,cfg)
-		if err!=nil{
-			fmt.Println("加载配置文件出错 , ",err)
+		err = yaml.Unmarshal(yaml_data, Cfg)
+		if err != nil {
+			fmt.Println("加载默认配置文件出错 , ", err)
 		}
 	}
-	config := &Config{
-		DbConfig{
-			"mysql",
-			"192.168.2.1",
-			"1111",
-		},
-		HttpServerConfig{
-			"192.168.2.1",
-			"11",
-		},
-	}
-	b,err := yaml.Marshal(config)
-	fmt.Println(string(b))
-	fmt.Println("init")
+	fmt.Println("加载默认配置文件成功")
 }
--- main.go
+++ main.go
@@ -6,14 +6,18 @@ import (
 	"go-blog/config"
 )
 
-func main(){
+func main() {
 
 	router := gin.Default()
 
 	//配置静态文件路径
-	router.Static("/public","./public")
-	cfg:= &config.Config{}
+	router.Static("/public", "./public")
+	cfg := &config.Config{}
 	fmt.Println(cfg)
 
-	router.Run(":2333")
+	//获取配置文件里的服务器连接地址
+	addr := config.Cfg.GetHttpAddr()
+	fmt.Println(addr)
+	//
+	router.Run(addr)
 }
